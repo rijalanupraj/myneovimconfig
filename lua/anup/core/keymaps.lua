3412752675e7d541213e205d3aa8815efa9f1960
Anup Raj Rijal <53175920+rijalanupraj@users.noreply.github.com>
2022-11-16 14:21:08 +0545

    add new keybindings to move faster up and down

--- lua/anup/core/keymaps.lua
+++ lua/anup/core/keymaps.lua
@@ -31,6 +31,11 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+-- Move Up and down faster
+keymap.set("n", "J", "5j")
+keymap.set("n", "K", "5k")
+keymap.set("n", "<leader>j", "J")
+
 ----------------------
 -- Plugin Keybinds
 ----------------------
